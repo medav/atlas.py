79f276f14db1f3355e10dc3b7aebf60c52e12d55
Michael Davies <michaelstoby@gmail.com>
2019-03-19 13:31:53 -0500

    [tests] Add example verilator testbench

---
+++ tests/veri_tb.py
@@ -0,0 +1,73 @@
+import sys
+sys.path.append('.')
+
+from atlas import *
+
+@Module
+def Gcd(data_width):
+    io = Io({
+        'in_a': Input(Bits(data_width)),
+        'in_b': Input(Bits(data_width)),
+        'start': Input(Bits(1)),
+        'out': Output(Bits(data_width)),
+        'done': Output(Bits(1))
+    })
+
+    a_reg = Reg(Bits(data_width))
+    b_reg = Reg(Bits(data_width))
+
+    with io.start:
+        a_reg <<= io.in_a
+        b_reg <<= io.in_b
+
+    with otherwise:
+        with a_reg > b_reg:
+            a_reg <<= a_reg - b_reg
+
+        with otherwise:
+            b_reg <<= b_reg - a_reg
+
+    io.done <<= (b_reg == 0)
+    io.out <<= a_reg
+
+    NameSignals(locals())
+
+circuit = Circuit('gcd', True, True)
+
+with Context(circuit):
+    top = Gcd(64)
+
+circuit.top = top
+
+tb = Testbench(circuit, VeriCompile(circuit, 'build'))
+
+def HwGcd(a, b):
+    tb.Reset(10)
+    tb.io.in_a <<= a
+    tb.io.in_b <<= b
+    tb.io.start <<= 1
+    tb.Step(1)
+    tb.io.start <<= 0
+
+    while tb.io.done.GetValue() == 0:
+        tb.Step(1)
+
+    return tb.io.out.GetValue()
+
+def SwGcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+
+    return a
+
+# print(HwGcd(0x100, 0x100))
+
+# tb.Step(10)
+
+for i in range(2, 1000):
+    for j in range(2, 1000):
+        s = SwGcd(i, j)
+        h = HwGcd(i, j)
+        if s >= 10:
+            print(s, '==', h)
+        assert s == h, f'Mismatch for Gcd({i}, {j})!'
